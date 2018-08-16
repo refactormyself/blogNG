0155619922a429c77bbf530092fb0bb459c54e1b
Bolarinwa Saheed <refactormyself@users.noreply.github.com>
2018-08-16 13:36:34 +0200

    BlogNG initial commit

---
+++ src/main/java/com/bolarinwa/apps/blogengine/BlogEngineApplication.java
@@ -0,0 +1,12 @@
+package com.bolarinwa.apps.blogengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogEngineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bolarinwa/apps/blogengine/BlogEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bolarinwa.apps.blogengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
